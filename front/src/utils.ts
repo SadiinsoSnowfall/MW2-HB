1b9ab491470cd0aa2e1bd833d656cabdbece206f
BuildTools <sadiinso@gmail.com>
2020-02-13 22:22:17 +0100

    Improved the Utils#Range function performance

--- front/src/utils.ts
+++ front/src/utils.ts
@@ -3,8 +3,33 @@
  * @param n the upper bound (non-inclusive) of the range
  * @returns A python-like range (o to $n-1)
  */
-export function range(n: number) {
-    return [...Array(n).keys()];
+export function* range(n: number) {
+    for (let i = 0; i < n; i++) {
+        yield i;
+    }
+}
+
+/** 
+ * @param from the lower bound of the range
+ * @param to the upper bound (non-inclusive) of the range
+ * @returns A python-like range (o to $n-1)
+ */
+export function* range2(from: number, to: number) {
+    for (let i = from; i < to; i++) {
+        yield i;
+    }
+}
+
+/** 
+ * @param from the lower bound of the range
+ * @param to the upper bound (non-inclusive) of the range
+ * @param step The step between two values of the range
+ * @returns A python-like range (o to $n-1)
+ */
+export function* range3(from: number, to: number, step: number) {
+    for (let i = from; i < to; i += step) {
+        yield i;
+    }
 }
 
 /** 
