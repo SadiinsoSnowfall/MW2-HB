a00d2f27a7539e3cc79ca834b94bcec5fdcd9c1b
BuildTools <sadiinso@gmail.com>
2020-02-20 00:47:39 +0100

    Added miscellaneous spritesheets

--- front/src/utils/assetsManager.ts
+++ front/src/utils/assetsManager.ts
@@ -5,7 +5,12 @@ export class Assets {
     private static assets = new Map<string, HTMLImageElement>(); 
 
     static readonly LEVELS_ICON = require('assets/images/levels_icon.png').default as string
+    static readonly MISC_MD = require('assets/images/misc_md.png').default as string
+    static readonly MISC_LG = require('assets/images/misc_lg.png').default as string
+    static readonly MISC_XL = require('assets/images/misc_xl.png').default as string
+    static readonly MISC_XLV = require('assets/images/misc_xlv.png').default as string
     static readonly BALLS = require('assets/images/balls.png').default as string
+    static readonly BALLS_SM = require('assets/images/balls_sm.png').default as string
     static readonly CUBES = require('assets/images/cubes.png').default as string
     static readonly TRIS = require('assets/images/tris.png').default as string
     static readonly FAT_PLANKS = require('assets/images/large_planks.png').default as string
@@ -25,7 +30,12 @@ export class Assets {
 
         return Promise.all([
             this.LEVELS_ICON,
+            this.MISC_MD,
+            this.MISC_LG,
+            this.MISC_XL,
+            this.MISC_XLV,
             this.BALLS,
+            this.BALLS_SM,
             this.CUBES,
             this.TRIS,
             this.FAT_PLANKS,
